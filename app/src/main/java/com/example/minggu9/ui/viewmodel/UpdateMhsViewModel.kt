25c9d7af7d059628b087965457e4ec792a3f95b4
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-15 15:14:58 +0700

    membuat fungsi updateState didalam class UpdateMhsViewModel

---
+++ app/src/main/java/com/example/minggu9/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.minggu9.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.minggu9.data.entity.Mahasiswa
+import com.example.minggu9.repository.RepositoryMhs
+import com.example.minggu9.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+
+    fun updateState (mahasiswaEvent: MahasiswaEvent) {
+        updateUIState = updateUIState.copy(
+            mahasiswaEvent = mahasiswaEvent,
+        )
+    }
+
+
+}
+
+fun Mahasiswa.toUIStateMhs() : MhsUIState = MhsUIState(
+    mahasiswaEvent = this.toDetailUiEvent(),
+)
