a0b0be9e9129ccf3d9d228291ecb20d3e398e02d
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-15 22:20:37 +0700

    membuat fungsi BodyHomeMhsView

--- app/src/main/java/com/example/minggu9/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/minggu9/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,8 +1,10 @@
 package com.example.minggu9.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,11 +14,16 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -26,8 +33,12 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.minggu9.data.entity.Mahasiswa
 import com.example.minggu9.repository.RepositoryMhs
 import com.example.minggu9.ui.viewmodel.HomeMhsViewModel
+import com.example.minggu9.ui.viewmodel.HomeUiState
 import com.example.minggu9.ui.viewmodel.PenyediaViewModel
 import com.example.monitoringapplication.ui.costumwidget.TopAppBar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeMhsView(
@@ -48,6 +59,68 @@ fun HomeMhsView(
         }
     ) {
 
+    }
+}
+
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+
+    val CoroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } // SnackBar State
+    when {
+        homeUiState.isLoading -> {
+            //Menampilkan indikator loading
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            // Menampilkan Pesan Error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    CoroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+
+        homeUiState.listMhs.isNotEmpty() -> {
+            // Menampilkan pesan jika data kosong
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data mahasiswa.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            // Menampilkan daftar Mahasiswa
+            ListMahasiswa(
+                listMhs = homeUiState.listMhs,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
     }
 }
 
