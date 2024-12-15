a7924743f41dd5fb613b0084587222648d9b7e91
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-15 14:46:24 +0700

    membuat data class DetailUiState

---
+++ app/src/main/java/com/example/minggu9/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.minggu9.ui.viewmodel
+
+class DetailMhsViewModel {
+
+}
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
