2b57e0d191ac691d0b2e8a3266d3861c35dc3e75
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-15 13:14:02 +0700

    membuat fungsi getAllMhs dari mahasiswa DAO untuk mendapatkan seluruh data mahasiswa dan getMhs dari mahasiswa DAO untuk mendapatkan data mahasiswa berdasarkan nim

--- app/src/main/java/com/example/minggu9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/minggu9/repository/RepositoryMhs.kt
@@ -4,6 +4,14 @@ import com.example.minggu9.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
+    // get All Mahaiswa
+    fun getAllMhs() : Flow<List<Mahasiswa>>
+
+    //getMhs
+    fun getMhs(nim: String) : Flow<Mahasiswa>
+
+
+
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMahasiswa() : Flow<List<Mahasiswa>>
 }
