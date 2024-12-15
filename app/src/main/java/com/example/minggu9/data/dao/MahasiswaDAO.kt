3d1aaafab7b85409563fe8f7b2bab0ded7eb2c57
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-15 13:05:27 +0700

    menambahkan fungsi delete dan update data mahasiswa pada DAO

--- app/src/main/java/com/example/minggu9/data/dao/MahasiswaDAO.kt
+++ app/src/main/java/com/example/minggu9/data/dao/MahasiswaDAO.kt
@@ -1,8 +1,10 @@
 package com.example.minggu9.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.minggu9.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -22,4 +24,10 @@ interface MahasiswaDAO {
     suspend fun insertMahasiswa(
         mahasiswa: Mahasiswa
     )
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
